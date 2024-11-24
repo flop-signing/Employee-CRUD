a44bfeb39146a5eb32ce546a9592e45362508e15
Mehedy <mehedy.mmc272@gmail.com>
2024-11-24 09:54:25 +0600

    First and Last Commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApllication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafdemoApllication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemoApllication.class, args);
+	}
+
+}
